7a3778abb051920d830a1e10f7c9a04db99f1406
anteqkois <anteqkois.dev@gmail.com>
2024-07-13 11:54:02 +0200

    (chore) clean public

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Improvement everyday 🚀",
+    pageTitle: "Better everyday 🚀",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
